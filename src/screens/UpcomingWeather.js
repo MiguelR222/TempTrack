69162c171d7249702e6a260869ae84741c099522
MiguelR222 <miguel.ruiz0652@gmail.com>
2023-11-23 18:43:10 -0600

    updated

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   SafeAreaView,
-  Text,
   StyleSheet,
   FlatList,
   View,
@@ -10,51 +9,7 @@ import {
 } from "react-native";
 import ListItem from "../Components/ListItem";
 
-const DATA = [
-  {
-    dt_txt: "2023-02-18 12:00:00",
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55,
-    },
-    weather: [
-      {
-        main: "Clear",
-      },
-    ],
-  },
-  {
-    dt_txt: "2023-02-18 15:00:00",
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55,
-    },
-    weather: [
-      {
-        main: "Clouds",
-      },
-    ],
-  },
-  {
-    dt_txt: "2023-02-18 18:00:00",
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55,
-    },
-    weather: [
-      {
-        main: "Rain",
-      },
-    ],
-  },
-];
-const Empty = () => (
-  <View>
-    <Text>Empty</Text>
-  </View>
-);
-
-const UpcomingWeather = () => {
+const UpcomingWeather = ({weatherData}) => {
   const renderItem = ({ item }) => (
     <ListItem
       condition={item.weather[0].main}
@@ -66,17 +21,16 @@ const UpcomingWeather = () => {
   const { container, image } = styles;
   return (
     <SafeAreaView style={container}>
-      {/* <ImageBackground source={require("WeatherApp/assets/clouds.jpg")} style={image}> */}
-        <Text>Upcoming Weather</Text>
+      <ImageBackground source={require("../../assets/clouds.jpg")} style={image}>
         <FlatList
-          data={DATA}
+          data={weatherData}
           renderItem={renderItem}
           keyExtractor={(item) => item.dt_txt}
           ItemSeparatorComponent={() => (
             <View style={{ backgroundColor: "red", height: 2 }} />
           )}
         />
-      {/* </ImageBackground> */}
+      </ImageBackground>
     </SafeAreaView>
   );
 };
